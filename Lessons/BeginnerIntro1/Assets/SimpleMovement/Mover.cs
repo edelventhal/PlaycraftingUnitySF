595f4239a1e5c9aa6780a70a3fa9798095dbb4c8
Eli <eli@lumoslabs.com>
2016-07-24 20:42:55 -0700

    Step 2. Add Time.deltaTime.

--- Lessons/BeginnerIntro1/Assets/SimpleMovement/Mover.cs
+++ Lessons/BeginnerIntro1/Assets/SimpleMovement/Mover.cs
@@ -13,7 +13,10 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     public void Update ()
     {
-        //move our Transform component's position to the right (X-axis) by 0.1 every frame
-        transform.position += new Vector3( 0.1f, 0.0f, 0.0f );
+        //since each frame may take up different amounts of time, multiply by Time.deltaTime
+        //in order to ensure smooth movement (uniform movement regardless of how long a frame takes)
+        //Time.deltaTime is a measure of how long the last frame took in seconds. It will typically
+        //be a very small value, somewhere near 1 / 60 (since you have 60 frames a second).
+        transform.position += new Vector3( 1.0f * Time.deltaTime, 0.0f, 0.0f );
     }
 }
