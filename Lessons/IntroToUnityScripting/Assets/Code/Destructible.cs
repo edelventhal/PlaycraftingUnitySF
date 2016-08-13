88423e45235289fe52a1775ca7b2b584dc530147
Eli <eli@lumoslabs.com>
2016-08-12 18:18:26 -0700

    IntroToUnityScripting imported images, made a physics controlled sprite, and put in a few comments.

--- Lessons/IntroToUnityScripting/Assets/Code/Destructible.cs
+++ Lessons/IntroToUnityScripting/Assets/Code/Destructible.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
+    public void Awake()
+    {
+        //everything here will happen IMMEDIATELY when the object is loaded or instantiated
+    }
+    
     public void Start()
     {
-        
+        //everything here will happen on the first frame update
     }
     
     public void Update()
     {
-        
+        //everything here will happen every frame
     }
 }
