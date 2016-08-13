d433a24d42f17d932a4e50e9892056ba3d8eabf3
Eli <eli@lumoslabs.com>
2016-08-13 13:49:48 -0700

    IntroToUnityScripting Implement the ability to only send collision messages once per collision.

--- Lessons/IntroToUnityScripting/Assets/Code/CollisionListener.cs
+++ Lessons/IntroToUnityScripting/Assets/Code/CollisionListener.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 //when a collision happens, sends messages to any attached CollisionHandlers
 public class CollisionListener : MonoBehaviour
 {
+    public bool onlySendOncePerCollision = true;
+    
+    protected List<Collider2D> activeColliders;
+    
+    public void Awake()
+    {
+        activeColliders = new List<Collider2D>();
+    }
+    
     public void OnTriggerEnter2D( Collider2D collider )
     {
         DoCollision( collider );
@@ -11,6 +21,11 @@ public class CollisionListener : MonoBehaviour
     public void OnTriggerStay2D( Collider2D collider )
     {
         DoCollision( collider );
+    }
+    
+    public void OnTriggerExit2D( Collider2D collider )
+    {
+        EndCollision( collider );
     }
     
     public void OnCollisionEnter2D( Collision2D collision )
@@ -23,11 +38,28 @@ public class CollisionListener : MonoBehaviour
         DoCollision( collision.collider );
     }
     
-    public void DoCollision( Collider2D collider )
+    public void OnCollisionExit2D( Collision2D collision )
     {
+        EndCollision( collision.collider );
+    }
+    
+    protected void DoCollision( Collider2D collider )
+    {
+        if ( onlySendOncePerCollision && activeColliders.Contains( collider ) )
+        {
+            return;
+        }
+        
         //this is a useful feature you can do in Unity – it will call any functions named "HandleCollision"
         //that exist on any MonoBehaviour's that are also attached to this GameObject.
         //As a warning – this is a bit inefficient. There are faster but less convenient ways to do this.
         SendMessage( "HandleCollision", collider );
+        
+        activeColliders.Add( collider );
+    }
+    
+    protected void EndCollision( Collider2D collider )
+    {
+        activeColliders.Remove( collider );
     }
 }
