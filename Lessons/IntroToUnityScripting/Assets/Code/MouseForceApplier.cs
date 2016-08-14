6bcb0427615dd9716a8f73e11e76a27af8253ee7
Eli <eli@lumoslabs.com>
2016-08-14 13:35:35 -0700

    IntroToUnityScripting - MouseForceApplier can now control faller.

---
+++ Lessons/IntroToUnityScripting/Assets/Code/MouseForceApplier.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MouseForceApplier : MonoBehaviour
+{
+    [Tooltip("This amount of force is applied in each dimension towards where the mouse is clicking.")]
+    public Vector2 appliedForce = new Vector2( 50.0f, 0.0f );
+    
+    [Tooltip("The force will be applied to this target object.")]
+    public Rigidbody2D target;
+    
+    //remember, physics will be applied from here, so we'll use FixedUpdate
+    public void FixedUpdate()
+    {
+        if ( Input.GetMouseButton( 0 ) )
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+            Vector2 forceDirection = ( (Vector2) ( mouseWorldPos - target.transform.position ) ).normalized;
+            Vector2 forceAmount = Vector2.Scale( appliedForce, forceDirection );
+            
+            //we must multiply by fixedDeltaTime since this is being applied every frame
+            target.AddForce( forceAmount * target.mass * Time.fixedDeltaTime );
+        }
+    }
+}
